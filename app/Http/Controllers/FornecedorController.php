6806d8dbf9130339c8dde15fff98640815b3137e
Guga1010 <joaogustavosdleo@gmail.com>
2023-10-26 11:53:37 -0300

    atualizando arquivos

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -12,13 +12,26 @@ class FornecedorController extends Controller
             0 => [
                 'nome' =>'Fornecedor 1', 
                 'status' => 'N', 
-                'cnpj' => '00.000.000/000-00'
+                'cnpj' => '0',
+                'ddd' => '',
+                'telefone' => '0000-0000'
             ],
             1 => [
                 'nome' =>'Fornecedor 2', 
                 'status' => 'S', 
+                'cnpj' => null,
+                'ddd' => '85',
+                'telefone' => '0000-0000'
+            ],
+            2 => [
+                'nome' =>'Fornecedor 3', 
+                'status' => 'S', 
+                'cnpj' => null,
+                'ddd' => '32',
+                'telefone' => '0000-0000'
             ]
         ];
+
         return view('app.fornecedor.index',compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,15 +1,18 @@
 <h3>Fornecedor</h3>
 
-@php
-@endphp
-
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}   
-    <br>
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{ $fornecedores[0]['cnpj'] }}
-    @endisset
+    @forelse ($fornecedores as $indice => $fornecedor)
+        Fornecedor: @{{ $fornecedor['nome'] }}
+        <br>
+        Status: @{{ $fornecedor['status'] }}   
+        <br>
+        CNPJ: @{{ $fornecedor['cnpj'] ?? '' }} 
+        <br>
+        Telefone: (@{{ $fornecedor['ddd'] ?? '' }}) @{{ $fornecedor['telefone'] ?? '' }}
+        <hr>   
+    @empty    
+        Não existem fornecedores cadastrados!!!
+    @endforelse
 @endisset
+
 
